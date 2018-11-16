6f586c3ee6740a9fa2b0026c50de403e90d8761d
yonathani <45083380+yonathani@users.noreply.github.com>
2018-11-15 16:10:28 -0800

    Initial commit

---
+++ app/src/main/java/com/example/yonathan/prueba/Prueba1.java
@@ -0,0 +1,13 @@
+package com.example.yonathan.prueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Prueba1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba1);
+    }
+}
---
+++ app/src/main/java/com/example/yonathan/prueba/Splashscreen.java
@@ -0,0 +1,23 @@
+package com.example.yonathan.prueba;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(Splashscreen.this,Prueba1.class);
+                startActivity(intent);
+
+            }
+        },6000);
+    }
+}
